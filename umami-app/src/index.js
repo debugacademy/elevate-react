6784512e4a461beeb627c66ac362730293dadf48
Ashraf Abed <Ashraf.Abed@debugacademy.com>
2020-08-04 23:42:05 -0400

    Umami app files init

---
+++ umami-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './task/App'
+
+export const DrupalContext = React.createContext(
+  {
+    siteUrl: 'http://website-URL-here/',
+    endpoint: '/path-to-API-endpoint-here (/jsonapi)'
+  });
+
+ReactDOM.render(<App />, document.getElementById('react-manage'));
